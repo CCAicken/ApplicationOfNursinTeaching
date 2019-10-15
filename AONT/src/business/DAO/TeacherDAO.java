757fc82309c0c453bc4af82ef12d26911fd564b3
mzGitH <37521264+mzGitH@users.noreply.github.com>
2019-10-15 16:20:29 +0800

    Revert "DAO更改"
    This reverts commit 65a0097f36a2882d6487fd5168ed1704d052277d.

--- AONT/src/business/DAO/TeacherDAO.java
+++ AONT/src/business/DAO/TeacherDAO.java
@@ -1,58 +1,9 @@
 package business.DAO;
-
-import java.util.List;
-
-import Model.TTeacher;
-
 /***
  * 
- * @author Aicken 教师模块接口设计
+ * @author Aicken
+ *教师模块接口设计
  */
 public interface TeacherDAO {
-	/**
-	 * 获取所有教师或管理员
-	 * 
-	 * @return
-	 */
-	public List<TTeacher> getAllTeachers();
 
-	/**
-	 * 根据用户类型获取对应教师或管理员
-	 * 
-	 * @param userTypeId
-	 * @return
-	 */
-	public List<TTeacher> getTeaByUserType(int userTypeId);
-
-	/**
-	 * 根据用户Id获取用户
-	 * 
-	 * @param userId
-	 * @return
-	 */
-	public TTeacher getTeaByTeaId(String userId);
-
-	/**
-	 * 添加教师用户
-	 * 
-	 * @param teacher
-	 * @return
-	 */
-	public int addTeacher(TTeacher teacher);
-
-	/**
-	 * 编辑教师信息
-	 * 
-	 * @param teacher
-	 * @return
-	 */
-	public boolean editTeacher(TTeacher teacher);
-
-	/**
-	 * 删除教师用户
-	 * 
-	 * @param teacher
-	 * @return
-	 */
-	public boolean delTeacher(TTeacher teacher);
 }
--- AONT/src/business/DAO/UserTypeDAO.java
+++ AONT/src/business/DAO/UserTypeDAO.java
@@ -1,25 +1,9 @@
 package business.DAO;
-
-import java.util.List;
-
-import Model.TUserType;
-
 /***
  * 
- * @author Aicken 用户类型模块接口设计
+ * @author Aicken
+ *用户类型模块接口设计
  */
 public interface UserTypeDAO {
-	/**
-	 * 获取所有用户类型
-	 * 
-	 * @return
-	 */
-	public List<TUserType> getAllUserType();
 
-	/**
-	 * 根据用户类型Id获取用户类型
-	 * 
-	 * @return
-	 */
-	public TUserType getUserTypeById();
 }
