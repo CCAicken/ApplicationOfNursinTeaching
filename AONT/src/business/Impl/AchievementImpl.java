8a7dcd38822c232e87f5e8bd44a48af18e1b086b
CCAicken <33279747+CCAicken@users.noreply.github.com>
2019-10-15 19:11:48 +0800

    考试视图修改

--- AONT/src/business/Impl/AchievementImpl.java
+++ AONT/src/business/Impl/AchievementImpl.java
@@ -2,6 +2,10 @@ package business.Impl;
 
 import java.util.List;
 
+import org.apache.log4j.chainsaw.Main;
+
+import Basic.iHibBaseDAO;
+import Basic.iHibBaseDAOImpl;
 import Model.TAchievement;
 import Model.VAchievement;
 import business.DAO.AchievementDAO;
@@ -11,11 +15,16 @@ import business.DAO.AchievementDAO;
  *
  */
 public class AchievementImpl implements AchievementDAO {
+	private iHibBaseDAO bdao;
+
+	public AchievementImpl() {
+		this.bdao = new iHibBaseDAOImpl();
+	}
 
 	@Override
 	public int Insert(TAchievement Achievement) {
-		// TODO Auto-generated method stub
-		return 0;
+		return 	(int) bdao.insert(Achievement);
+		
 	}
 
 	@Override
@@ -60,6 +69,10 @@ public class AchievementImpl implements AchievementDAO {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	 
+	public static void main(String[] args) {
+		 
 
+	}
  
 }
